a8d7965ca8d5883b1d06bf0abf3a171d740cf9fc
Dhiraj Pradhan <dpr4dhan@gmail.com>
2025-09-16 17:17:48 +0545

    Fullstackopen - Exercise 2.1 - 2.5

--- src/App.jsx
+++ src/App.jsx
@@ -1,64 +1,59 @@
-import {useState} from "react";
-import Button from "./components/Button.jsx";
+import Course from "./components/Course.jsx";
 
+const App = () => {
+    const courses = [
+        {
+            name: 'Half Stack application development',
+            id: 1,
+            parts: [
+                {
+                    name: 'Fundamentals of React',
+                    exercises: 10,
+                    id: 1
+                },
+                {
+                    name: 'Using props to pass data',
+                    exercises: 7,
+                    id: 2
+                },
+                {
+                    name: 'State of a component',
+                    exercises: 14,
+                    id: 3
+                },
+                {
+                    name: 'Redux',
+                    exercises: 11,
+                    id: 4
+                }
+            ]
+        },
+        {
+            name: 'Node.js',
+            id: 2,
+            parts: [
+                {
+                    name: 'Routing',
+                    exercises: 3,
+                    id: 1
+                },
+                {
+                    name: 'Middlewares',
+                    exercises: 7,
+                    id: 2
+                }
+            ]
+        }
+    ]
 
-const StatisticsLine = ({text, value}) => {
-    return <tr><td>{text}</td><td>{value}</td></tr>
-}
-const Statistics = ({good, neutral, bad}) => {
-
-    if(good === 0 && neutral === 0 && bad === 0 ){
-        return <div>No any feedbacks</div>
-    }
-
-    const total = good + neutral + bad;
-    const avg = (good - bad) / (good + bad);
-
-    const positive = ((good + neutral) / total ) * 100;
 
     return (
-        <div>
-            <table>
-                <tbody>
-                <StatisticsLine text="Good" value={good}/>
-                <StatisticsLine text="Neutral" value={neutral}/>
-                <StatisticsLine text="Bad" value={bad}/>
-                <StatisticsLine text="Total" value={total}/>
-                <StatisticsLine text="Average" value={avg}/>
-                <StatisticsLine text="Positive" value={positive}/>
-                </tbody>
+        <>
+        {
+            courses.map((course) => <Course course={course} key={course.id} />)
+        }
+        </>
 
-            </table>
-        </div>
-    )
-}
-
-function App() {
-    const [good, setGood] = useState(0)
-    const [neutral, setNeutral] = useState(0)
-    const [bad, setBad] = useState(0)
-
-    const handleGood = () => {
-        setGood(prev => prev + 1);
-    }
-
-    const handleNeutral = ()=>{
-        setNeutral(prev => prev + 1);
-    }
-
-    const handleBad = () =>{
-        setBad(prev => prev + 1);
-    }
-
-    return (
-        <div>
-            <Button text="Good" onClick={handleGood}/>
-            <Button text="Neutral" onClick={handleNeutral}/>
-            <Button text="Bad" onClick={handleBad}/>
-            <br/>
-            <h4>Statistics</h4>
-            <Statistics good={good} bad={bad} neutral={neutral}/>
-        </div>
     )
 }
 
--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,11 +1,14 @@
 import Part from "./Part.jsx";
 
 const Content = ({parts}) => {
+    console.log('parts',parts);
+    const total = parts.reduce((sum, part) =>  sum + part.exercises, 0);
     return (
         <>
             {
-                parts.map((part) => <Part part={part}/>)
+                parts.map((part) => <Part part={part} key={part.id}/>)
             }
+            <b>Total of {total} exercises.</b>
         </>
     )
 }
